e76bf50aa26ad7ab23a3d6e44a182eb4de32fc7b
simonethan648-prog <simonethan648@gmail.com>
2026-06-07 10:48:54 +0400

    Add files via upload
    Here, you have to create a rock paper scissor game, where user will make one move, and the program will make the next move

---
+++ Main2.py
@@ -0,0 +1,26 @@
+import random
+import pygame
+
+class Button():
+    def __init__(self, x, y, pos, width, height):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.pos = pos
+    def clicked(self, pos):
+        self.pos = pygame.mouse.get_pos()
+        if self.pos[0] > self.x and self.pos[0] < self.x + self.width:
+            if self.pos[1] > self.y and self.pos[1] < self.y + self.height:
+                return True
+            return False
+        class RpsGame():
+            def __init__(self):
+                pygame.init()
+                self.screen = pygame.display.set_mode((960, 640))
+                pygame.display.set_caption("RPS Smasher")
+                self.bg = pygame.image.load("background.png")
+                self.rock_btn = pygame.image.load("rock.png")
+                self.paper_btn = pygame.image.load("paper.png")
+                self.scissor_btn = pygame.image.load("scissor.png")
+                
